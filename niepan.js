d5d2fcafacbe78065071ed49887c6997bd2cf2f3
yegao <familyink@163.com>
2018-03-30 12:44:01 +0800

    do something for load the script serval times and add copyright as yegao

---
+++ niepan.js
@@ -0,0 +1,21 @@
+(function(global,undefined){
+  //防止重复加载niepan.js
+  if(global.niepan && global.niepan.copyright==='@yegao'){
+    return;
+  }
+  //达到np()等价于new np()的目的
+  var np = function(){
+    return np.prototype.create(np.prototype);
+  }
+  np.prototype = {
+    copyright:'@yegao',
+    create:function(parent,init){
+      if(typeof init != 'function'){
+        init = function(){};
+      }
+      init.prototype = parent;
+      return new init;
+    }
+  }
+  global.niepan = np;
+})(window,undefined);
