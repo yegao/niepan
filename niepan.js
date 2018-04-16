b25363e0f1c5dde2877fd4eae13d30effd2bf785
yegao <familyink@163.com>
2018-04-16 11:06:08 +0800

    comment

--- niepan.js
+++ niepan.js
@@ -1,18 +1,27 @@
 (function(global, undefined) {
+
   //防止重复加载niepan.js
   if (global.niepan && global.niepan.copyright === '@yegao') {
     return;
   }
+
   //达到np()等价于new np()的目的
   var np = function(element = null) {
     return np.prototype.create(np.prototype, element);
   }
+
   //原型属性
   np.prototype = {
     //内部静态变量,最好不要动
     version: '1.0.7',
     copyright: '@yegao',
-    //@TODO 抽象语法树
+    /**
+     * @todo 抽象语法树
+     * @param  {[type]} element [description]
+     * @param  {[type]} _index  [description]
+     * @param  {[type]} _res    [description]
+     * @return {[type]}         [description]
+     */
     ast:function(element,_index,_res){
       // if(Object.prototype.toString.call(element) === "[object Array]"){
       //   console.warn('the ast get multiple trees');
@@ -21,15 +30,12 @@
       if(!element){
         return;
       }
-/*
-·[3]
-·[2]            ·[2]                            ·[4]
-·[3]            ·[3]            ·[3]            ·[2]        ·[2]        ·[0]    ·[1]
-·[0]·[0]·[0]    ·[0]·[0]·[0]    ·[0]·[0]·[0]    ·[0]·[0]    ·[0]·[0]            ·[0]
-*/
-
-
-
+/********************************************************************************************
+ *     ·[3]                                                                                 *
+ *     ·[2]            ·[2]                            ·[4]                                 *
+ *     ·[3]            ·[3]            ·[3]            ·[2]        ·[2]        ·[0]    ·[1] *
+ *     ·[0]·[0]·[0]    ·[0]·[0]·[0]    ·[0]·[0]·[0]    ·[0]·[0]    ·[0]·[0]            ·[0] *
+ ********************************************************************************************/
       var index = _index || 0;
       var res = _res || [];
       //nodeType 1的比如img的childElementCount为0 nodeType为3的txt没有childElementCount
@@ -49,7 +55,12 @@
       });
       return res;
     },
-    //@TODO需不需要将插入的子节点转成niepan,或者是添加一个参数来控制?
+    /**
+     * @todo需不需要将插入的子节点转成niepan,或者是添加一个参数来控制?
+     * @param  {[object|string]} elementOrString [description]
+     * @param  {[object]} parentNode      [description]
+     * @return {[object]}                 [description]
+     */
     append: function(elementOrString, parentNode) {
       if (!parentNode || typeof parentNode != 'object') {
         throw new Error('you append a element to a undefined parent node');
@@ -64,7 +75,13 @@
           throw new Error('you append a element to a undefined parent node');
       }
     },
-    //@TODO 是否需要合并重复的niepan,脏检查或者直接将元素上的事件就直接放到元素上，仔细一想也没什么必要...待定
+    /**
+     * 以prototype为原型创建一个对象，该对象的element属性为element
+     * @todo 是否需要合并重复的niepan,脏检查或者直接将元素上的事件就直接放到元素上，仔细一想也没什么必要...待定
+     * @param  {object} prototype 原型
+     * @param  {object} element   niepan对绑定的元素
+     * @return {object}           生成的niepan
+     */
     create: function(prototype, element) {
       // 实例属性
       var init = function() {
@@ -87,10 +104,12 @@
       init.prototype = prototype;
       return new init;
     },
-    // 事件
-    /**
+   /**
     * 通过sub注册的事件如果是元素自带的系统事件，既可以通过pub触发也可以通过系统方式(比如点击鼠标)触发
     * 通过sub注册的事件如果是自定义的事件，只能通过pub触发
+    * @param  {[type]}   event    事件名
+    * @param  {Function} callback 事件被触发后执行的回调方法
+    * @param  {[type]}   once     该事件被触发一次之后是否被销毁
     */
     sub: function(event, callback, once) {
       console.log(this.element, event);
@@ -115,6 +134,10 @@
         }
       }
     },
+    /**
+     * 触发事件
+     * @param  {string} event 事件名
+     */
     pub: function(event) {
       if (this.listeners[event]) {
         this.listeners[event].callback();
@@ -126,10 +149,18 @@
         console.warn('not found event \'' + event + '\',maybe it has been removed');
       }
     },
+    /**
+     * 绑定事件,该事件只能被触发一次就会被销毁
+     * @param  {object}   event    [description]
+     * @param  {Function} callback [description]
+     */
     once: function(event, callback) {
       this.sub(event, callback, true);
     },
-    // 请求
+    /**
+     * 发送http请求
+     * @param  {object} o [description]
+     */
     request: function(o) {
       if (!o.url) {
         throw new Error('niepan.request need url!');
@@ -168,7 +199,11 @@
         throw new Error("current environment does not support XMLHTTP!");
       }
     },
-    // 动画
+    /**
+     * 动画
+     * @param  {Function} fn   [description]
+     * @param  {number}   time [description]
+     */
     animate:function(fn,time){
       function step(){
         if(typeof fn === 'function'){
@@ -196,9 +231,13 @@
     //     url:''
     //   }
     // },
-    // MutationObserver 的不同兼容性写法
-    // 该构造函数用来实例化一个新的 Mutation 观察者对象
-    // Mutation 观察者对象能监听在某个范围内的 DOM 树变化
+
+    /**
+     * MutationObserver 的不同兼容性写法
+     * 该构造函数用来实例化一个新的 Mutation 观察者对象
+     * Mutation 观察者对象能监听在某个范围内的 DOM 树变化
+     * @type {[type]}
+     */
     obeserve:function(){
       var MutationObserver = global.MutationObserver || global.WebKitMutationObserver || global.MozMutationObserver;
       var observer = new MutationObserver(function(mutations) {
@@ -225,13 +264,18 @@
         childList: true
       });
     },
-    // 防止xss攻击
+    /**
+     * 防止xss攻击
+     * @type {Object}
+     */
     notxss:{
       keywords:[],//检测的关键字符
       withelist:[],//白名单列表
       blacks:[],//黑名单列表
     }
   }
+
+
   //global、amd、cmd、Commonjs
   if (global) {
     global.niepan = np;
@@ -250,4 +294,6 @@
   } else {
     throw new Error('current environment do not support niepan');
   }
+
+
 })(window || this);
