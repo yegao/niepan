f9ac9f786d3daabb8986ac44c4700f30e793ca1d
yegao <familyink@163.com>
2018-05-02 17:40:46 +0800

    module.exports

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,89 @@
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const config = require('../config')
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  context: path.resolve(__dirname, '../'),
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    path: config.build.assetsRoot,//path.resolve(__dirname, '../dist')
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath    //'/'
+      : config.dev.assetsPublicPath      //'/develop'
+  },
+  resolve: {
+    //require('./a')就可能去找./a.js、./a.vue、./a.json
+    extensions: ['.js', '.np', '.json'],
+    //require('@/a')相当于require('src/a')
+    alias: {
+      '@': resolve('src'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.np$/,
+        loader: 'np-loader',
+        // options:
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        //只转化src、node_modules/webpack-dev-server/client这些目录下的js文件
+        include: [resolve('src'), resolve('node_modules/webpack-dev-server/client')]
+      },
+      //图片
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      //音频
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      //字体
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+  //node中每个属性都是 Node.js 全局变量或模块的名称
+  //每一项的设置值都可以是（true/mock/empty/false）中的一种
+  //以确定这些node中的对象在其它环境中是否可用
+  node: {
+    // prevent webpack from injecting useless setImmediate polyfill because Vue
+    // source contains it (although only uses it if it's native).
+    // 防止webpack注入无用的setImmediate polyfill，因为Vue的源码中已经包含了这些
+    setImmediate: false,
+    // prevent webpack from injecting mocks to Node native modules
+    // that does not make sense for the client
+    // 防止webpack向Node原生模块中注入模拟代码,这个对客户端没有意义
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,44 @@
+var content = require('./index.np');
+console.log(content);
+const niepan = require('./niepan.js');
+console.log(niepan);
+var o = niepan();
+console.log(o);
+
+o.sub('xx',function(){console.log('pub...')});
+o.pub('xx');
+o.pub('xx');
+
+o.sub('yy',function(){console.log('pub...')},true);
+o.pub('yy');
+o.pub('yy');
+
+o.request({
+  url:'http://localhost:8888',
+  success:function(res){
+    console.log(res);
+  }
+});
+
+var li1 = niepan(document.getElementById('li1'));
+console.log(li1);
+li1.sub('click',function(){
+  console.log('clicked li1,this event could be pubed all the time');
+});
+
+var body= niepan(document.body);
+console.log(body);
+body.sub('click',function(){
+  console.log('clicked body,this event would be pubed only one time');
+},true);
+
+var li3 = niepan(document.getElementById('li3'));
+console.log(li3.$data.age);
+
+var input1 = niepan(document.getElementById('input1'));
+input1.sub('input',function(evt){
+  console.log(input1.$data.name);
+});
+
+// It will throw a error when define a niepan for the same element moe than one time;
+// var input2 = niepan(document.getElementById('input1'));//ERROR
